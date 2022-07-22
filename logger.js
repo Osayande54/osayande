79910133484fa32f4338c7ccee2035d9e7938a40
Bramwell Osayande <bramwellosayande@gmail.com>
2022-07-22 04:09:36 +0100

    first commit

--- logger.js
+++ logger.js
@@ -1,26 +1,23 @@
-var plow = require("fs");
-plow.writeFile("example.txt", "this is an example", (err) => {
+const Vlog = require("fs");
+const hunger = Vlog.mkdir("input.txt", (data, err) => {
   if (err) {
-    console.log("opps!!! something went wrong");
+    console.log(err);
   } else {
-    console.log("Congrats!!! operation was successful");
+    console.log("this was done succesfully");
   }
 });
-var flow = require("fs");
-flow.appendFile(
-  "example.js",
-  "I am currently at the Etihad stadium in Manchester",
-  (err) => {
-    if (err) {
-      console.log("error has occured once more");
-    } else {
-      console.log("Whoolala");
-    }
+const read = require("fs");
+const shop = read.readFile("example.txt", (err, data) => {
+  if (err) {
+    console.log("Something went wrong");
+  } else {
+    console.log("Operation read the file was succesful");
   }
-);
-// const fold = require("node:path");
-// const sugar = fold.normalize('C:\Users\HP\projects\osayande\logger.js')
-// console.log(sugar)
-const blem = require("os");
-const vlog = blem.release();
-console.log(vlog)
+});
+console.log(__filename);
+console.log(__dirname);
+function printHello(){
+  console.log("We go de alright na just time e go take")
+}
+setTimeout(printHello, 7000); 
+
